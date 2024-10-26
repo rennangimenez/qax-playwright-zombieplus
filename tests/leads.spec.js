e1a1e852d7c589f0864042091c4732015ade8176
RGColozzo <rennan.colozzo18@hotmail.com>
2024-10-26 00:30:19 -0300

    adding wrong email validation

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -25,3 +25,20 @@ test('must register a lead in the waiting queue', async ({ page }) => {
 
   await expect(page.locator('.toast')).toBeHidden({ timeout: 5000 })
 });
+
+test('must not register a lead with wrong email', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click()
+
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu nome completo').fill('Rennan Gimenez')
+  await page.getByPlaceholder('Seu email principal').fill('rennan.gimenez')
+
+  await page.getByTestId('modal').getByText('Quero entrar na fila!').click()
+
+  await expect(page.locator('.alert')).toHaveText('Email incorreto')
+});
