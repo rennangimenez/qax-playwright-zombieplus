d794783e4b4f26b0a1aff2235eb6d1c7d160077d
RGColozzo <rennan.colozzo18@hotmail.com>
2024-10-26 00:25:02 -0300

    adding first leads test

---
+++ tests/leads.spec.js
@@ -0,0 +1,27 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('must register a lead in the waiting queue', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click()
+
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu nome completo').fill('Rennan Gimenez')
+  await page.getByPlaceholder('Seu email principal').fill('contato.rennang@gmail.com')
+
+  await page.getByTestId('modal').getByText('Quero entrar na fila!').click()
+
+  // This is a way to get the HTML from the page in the same time the toast is visible on it
+  /*await page.getByText('seus dados conosco').click()
+  const content = await page.content()
+  console.log(content)*/
+
+  const message = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
+  await expect(page.locator('.toast')).toHaveText(message)
+
+  await expect(page.locator('.toast')).toBeHidden({ timeout: 5000 })
+});
